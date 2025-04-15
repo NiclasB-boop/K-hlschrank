d89c22456994caf3b36add1c8b004818d380b421
DerIch69420 <netroMorten@web.de>
2025-04-15 18:03:48 +0200

    Enhanced structure

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const audio = new Audio()
-audio.src = "sound1.mp3"
+const audio = new Audio();
+audio.src = "sound1.mp3";
 const image = document.getElementById("image");
 function animation() {
-    image.removeEventListener("click",animation)
-    image.src = "Test.gif?" + new Date().getTime();
-    setTimeout(animationEnd, 36*120)
+    image.removeEventListener("click", animation);
+    image.src = "assets/box.gif?" + new Date().getTime();
+    setTimeout(animationEnd, 30 * 120);
 }
 
 function animationEnd() {
-    image.src = "Test_end.png"
-    image.addEventListener("click",Sound)
+    image.src = "assets/box_end.png";
+    image.addEventListener("click", Sound);
 }
 function setAudioTime() {
     audio.currentTime = document.getElementById("Zeit").value;
-    document.getElementById("zeitLabel").innerText = document.getElementById("Zeit").value
+    document.getElementById("zeitLabel").innerText =
+        document.getElementById("Zeit").value;
 }
 function Sound() {
-    if (audio.paused){
-        audio.play()
+    if (audio.paused) {
+        audio.play();
     } else {
-        audio.pause()
+        audio.pause();
     }
-    
 }
-image.addEventListener("click",animation)
+image.addEventListener("click", animation);
