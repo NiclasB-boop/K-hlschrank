61d385a62236f91e3460c9018be2fff1eaa5901e
NiclasB-boop <niclas.barthel@mrge.lernsax.de>
2025-04-15 17:45:55 +0200

    intinal commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const audio = new Audio()
+audio.src = "sound1.mp3"
+const image = document.getElementById("image");
+function animation() {
+    image.removeEventListener("click",animation)
+    image.src = "Test.gif?" + new Date().getTime();
+    setTimeout(animationEnd, 36*120)
+}
+
+function animationEnd() {
+    image.src = "Test_end.png"
+    image.addEventListener("click",Sound)
+}
+function setAudioTime() {
+    audio.currentTime = document.getElementById("Zeit").value;
+    document.getElementById("zeitLabel").innerText = document.getElementById("Zeit").value
+}
+function Sound() {
+    if (audio.paused){
+        audio.play()
+    } else {
+        audio.pause()
+    }
+    
+}
+image.addEventListener("click",animation)
